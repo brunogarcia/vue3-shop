f9dd6e527bbaecf6fa45ad4ae9e67ad46dbbc1e8
Bruno Garcia <bruno@garciaechegaray.com>
2021-02-03 21:01:40 +0100

    [feat] The checkout can scan a product

--- src/middleware/checkout.ts
+++ src/middleware/checkout.ts
@@ -1,12 +1,47 @@
-import discountRules from "@/middleware/discountRules";
+import PRODUCT from "@/enums/product";
 import { PricingRule, DiscountRule } from "@/types";
+import discountRules from "@/middleware/discountRules";
+
+const { EMPTY } = PRODUCT;
 
 export default class Checkout {
   private pricingRules: PricingRule[];
   private discountRules: DiscountRule[] = [];
+  private emptyPricingRule: PricingRule = {
+    code: EMPTY,
+    price: 0,
+    discounts: []
+  };
 
   constructor(pricingRules: PricingRule[]) {
     this.pricingRules = pricingRules;
     this.discountRules = discountRules();
+  }
+
+  /**
+   * Scan a product
+   *
+   * @public
+   * @param {PRODUCT} code - The pricing rule code
+   * @returns {this}
+   */
+  public scan(code: PRODUCT): this {
+    const pricingRule = this.getPricingRule(code);
+    console.log(pricingRule);
+
+    return this;
+  }
+
+  /**
+   * Commons - Get princing rule
+   *
+   * @param {PRODUCT} code - The product code
+   * @returns {PricingRule}
+   */
+  private getPricingRule(code: PRODUCT): PricingRule {
+    const pricingRule: PricingRule | undefined = this.pricingRules.find(
+      item => item.code === code
+    );
+    return pricingRule || this.emptyPricingRule;
   }
 }
