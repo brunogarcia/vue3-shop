44615f12dc57d6896c9b2765f45c412fdc7515cd
Bruno Garcia <bruno@garciaechegaray.com>
2021-02-03 22:03:54 +0100

    [feat] Create shopping mutations

--- src/store/shopping/index.ts
+++ src/store/shopping/index.ts
@@ -1,10 +1,11 @@
 import initialState from "@/store/shopping/initialState";
 import getters from "@/store/shopping/getters";
+import mutations from "@/store/shopping/mutations";
 
 export default {
   namespaced: true,
   state: initialState(),
   actions: {},
-  mutations: {},
+  mutations,
   getters
 };
