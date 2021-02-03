b99a79877ea521a64533709fb0b46d273b55725b
Bruno Garcia <bruno@garciaechegaray.com>
2021-02-03 22:49:28 +0100

    [feat] Create component: summary discounts

--- tests/unit/views/Summary.spec.ts
+++ tests/unit/views/Summary.spec.ts
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import Summary from "@/views/Summary.vue";
 import Title from "@/components/Title.vue";
 import SummaryItems from "@/components/SummaryItems.vue";
+import SummaryDiscounts from "@/components/SummaryDiscounts.vue";
 
 describe("Summary.vue", () => {
   it("Must use Title component", () => {
@@ -12,5 +13,10 @@ describe("Summary.vue", () => {
   it("Must use SummaryItems component", () => {
     const wrapper = shallowMount(Summary);
     expect(wrapper.findComponent(SummaryItems).exists()).toBeTruthy;
+  });
+
+  it("Must use SummaryDiscounts component", () => {
+    const wrapper = shallowMount(Summary);
+    expect(wrapper.findComponent(SummaryDiscounts).exists()).toBeTruthy;
   });
 });
