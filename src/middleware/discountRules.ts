2f984868344a37841dd93e531cd1896a22f704e4
Bruno Garcia <bruno@garciaechegaray.com>
2021-02-03 20:52:36 +0100

    [feat] The checkout manages the discount rules

---
+++ src/middleware/discountRules.ts
@@ -0,0 +1,58 @@
+import PRODUCT from "@/enums/product";
+import DISCOUNT from "@/enums/discount";
+import { DiscountRule, ScannerItem } from "@/types";
+
+const { CAP, TSHIRT } = PRODUCT;
+const { TWO_X_ONE, BULK, PROMO_CODE } = DISCOUNT;
+
+/**
+ * Discount rules
+ *
+ * @returns {DiscountRule[]}
+ */
+export default function discountRules(): DiscountRule[] {
+  return [
+    /**
+     * Discount "2x1"
+     * Buy 2 of the same product, get one free
+     */
+    {
+      code: TWO_X_ONE,
+      literal: "2x1 Mug offer",
+      products: [CAP],
+      minToApply: 2,
+      calculateDiscount(item: ScannerItem): number {
+        const discountPerItem = item?.price ? (item.price * 50) / 100 : 0;
+        return item.counter * discountPerItem;
+      }
+    },
+
+    /**
+     * Discount "Bulk"
+     * Buying 3 or more, the price per unit is reduced 5%
+     */
+    {
+      code: BULK,
+      literal: "x3 Shirt offer",
+      products: [TSHIRT],
+      minToApply: 3,
+      calculateDiscount(item: ScannerItem): number {
+        const discountPerItem = item?.price ? (item.price * 5) / 100 : 0;
+        return item.counter * discountPerItem;
+      }
+    },
+
+    /**
+     * Promo code
+     */
+    {
+      code: PROMO_CODE,
+      literal: "Promo Code",
+      products: [],
+      minToApply: 0,
+      calculateDiscount(): number {
+        return 0;
+      }
+    }
+  ];
+}
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,26 @@
 import PRODUCT from "@/enums/product";
 import DISCOUNT from "@/enums/discount";
 
+export interface PricingRule {
+  code: PRODUCT;
+  price: number;
+  discounts: DISCOUNT[];
+}
+
 export interface Product extends PricingRule {
   id: string;
   name: string;
   currency: string;
 }
 
-export interface PricingRule {
-  code: PRODUCT;
-  price: number;
-  discounts: DISCOUNT[];
+export interface ScannerItem extends PricingRule {
+  counter: number;
+}
+
+export interface DiscountRule {
+  code: DISCOUNT;
+  literal: string;
+  products: PRODUCT[];
+  minToApply: number;
+  calculateDiscount(item: ScannerItem): number;
 }
