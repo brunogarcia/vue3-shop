d12d446b0c0996293a657ae82091289b86f2a71f
Bruno Garcia <bruno@garciaechegaray.com>
2021-02-03 21:13:24 +0100

    [feat] The checkout knows the total items

--- src/middleware/checkout.ts
+++ src/middleware/checkout.ts
@@ -1,11 +1,12 @@
 import PRODUCT from "@/enums/product";
-import { PricingRule, DiscountRule } from "@/types";
+import { PricingRule, DiscountRule, ScannerItem } from "@/types";
 import discountRules from "@/middleware/discountRules";
 
 const { EMPTY } = PRODUCT;
 
 export default class Checkout {
   private totalCost = 0;
+  private scanner: ScannerItem[] = [];
   private pricingRules: PricingRule[];
   private discountRules: DiscountRule[] = [];
   private emptyPricingRule: PricingRule = {
@@ -30,6 +31,16 @@ export default class Checkout {
   }
 
   /**
+   * Get total items
+   *
+   * @public
+   * @returns {number}
+   */
+  public getTotalItems(): number {
+    return this.scanner.length;
+  }
+
+  /**
    * Scan a product
    *
    * @public
@@ -38,7 +49,9 @@ export default class Checkout {
    */
   public scan(code: PRODUCT): this {
     const pricingRule = this.getPricingRule(code);
+
     this.addPriceToTotal(pricingRule);
+    this.addItemToScanner(pricingRule);
 
     return this;
   }
@@ -86,5 +99,78 @@ export default class Checkout {
    */
   private subtractPriceToTotal(product: PricingRule) {
     this.totalCost -= product.price;
+  }
+
+  /**
+   * Scanner- Add item to scanner
+   *
+   * @param {PricingRule} product - The product to add
+   */
+  private addItemToScanner(product: PricingRule) {
+    const indexItem = this.getItemIndexFromScannerByCode(product.code);
+    const existItem = indexItem >= 0;
+
+    if (existItem) {
+      this.sumOneToItemScannerCounter(indexItem);
+    } else {
+      this.addNewItemToScanner(product);
+    }
+  }
+
+  /**
+   * Scanner- Get item index from scanner by code
+   *
+   * @param {PRODUCT} code - The product code
+   * @returns {number}
+   */
+  private getItemIndexFromScannerByCode(code: PRODUCT): number {
+    return this.scanner.findIndex(item => item.code === code);
+  }
+
+  /**
+   * Scanner- Sum one to item counter
+   *
+   * @param {number} indexItem - The item index on the scanner list
+   */
+  private sumOneToItemScannerCounter(indexItem: number) {
+    const counter = this.scanner[indexItem].counter + 1;
+    this.updateItemScannerCounter(indexItem, counter);
+  }
+
+  /**
+   * Scanner- Update item counter
+   *
+   * @param {number} indexItem - The product index on the scanner list
+   * @param {number} counter - The item counter
+   */
+  private updateItemScannerCounter(indexItem: number, counter: number) {
+    const currentItem = this.scanner[indexItem];
+
+    const itemsFiltered = this.scanner.filter(
+      item => item.code !== currentItem.code
+    );
+
+    this.scanner = [
+      ...itemsFiltered,
+      {
+        ...currentItem,
+        counter
+      }
+    ];
+  }
+
+  /**
+   * Scanner- Add new product to scanner
+   *
+   * @param {PricingRule} product - The product to add
+   */
+  private addNewItemToScanner(product: PricingRule) {
+    this.scanner = [
+      ...this.scanner,
+      {
+        ...product,
+        counter: 1
+      }
+    ];
   }
 }
---
+++ tests/unit/middleware/checkout-total-items.spec.ts
@@ -0,0 +1,23 @@
+import PRODUCT from "@/enums/product";
+import Checkout from "@/middleware/checkout";
+import mockPrincingRules from "../mocks/mockPrincingRules";
+
+const { CAP, TSHIRT, MUG } = PRODUCT;
+
+describe("Checkout - Total items", () => {
+  let checkout: Checkout;
+  const princingRules = mockPrincingRules();
+
+  beforeEach(() => {
+    checkout = new Checkout(princingRules);
+  });
+
+  it("After scan a list of products, the total items must be updated", () => {
+    checkout
+      .scan(CAP)
+      .scan(TSHIRT)
+      .scan(MUG);
+
+    expect(checkout.getTotalItems()).toBe(3);
+  });
+});
